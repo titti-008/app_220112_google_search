ceec7c39e533d90880fcd67a3f200467d1612907
titti-008 <ikuzah_m2005@yahoo.co.jp>
2022-01-18 23:21:15 +0900

    210118_fix_system_spec

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
 
-  describe "GET #home" do
-    it "returns http success" do
+  context "GET #home" do
+    before do
       get :home
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
+
   end
 
-  describe "GET #show" do
+  context "GET #show" do
+    before do
+      get :show, params: {search_word: "チェンソーマン", start_index: 1}
+    end
+
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
   end
